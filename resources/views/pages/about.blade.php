58881208395e5db267188ce549b98a0e14e7a31e
Mohammad Talha <mohammadtalha91@gmail.com>
2019-02-25 21:08:04 +0500

    About page content updated

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
     <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
 		<div class="my-auto">
 			<h1>About <span class="text-primary"> Quran and Life</span></h1>
-			<h3>Open Source Project.</h3>
+			<h2>Open Source Project. Copyrights @ Mohammad Talha (Email: mohammadtalha91@gmail.com)</h2>
 		</div>
     </section>
 
