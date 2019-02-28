585c4eb195054de810540c0103e7d9f4ec6561fa
Mohammad Talha <mohammadtalha91@gmail.com>
2019-02-28 18:24:42 +0500

    add facebook analytics sdk in footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -9,3 +9,26 @@
 <script src="{{ url('/public/js') }}/resume.min.js"></script>
 <!-- html2canvas -->
 <script src="{{ url('/public/js') }}/html2canvas.min.js"></script>
+
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1014092718778022',
+      cookie     : true,
+      xfbml      : true,
+      version    : '3.2'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
