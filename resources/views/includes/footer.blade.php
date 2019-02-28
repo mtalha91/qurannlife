8997274d595cbea9ac694e0a0eb296e58fe5f447
Mohammad Talha <mohammadtalha91@gmail.com>
2019-02-28 21:37:17 +0500

    add firebase script in the footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,15 +1,16 @@
-<!-- Bootstrap core JavaScript -->
-<script src="{{ url('/public/vendor') }}/jquery/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="{{ url('/public/vendor') }}/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Plugin JavaScript -->
-<script src="{{ url('/public/vendor') }}/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for this template -->
-<script src="{{ url('/public/js') }}/resume.min.js"></script>
-<!-- html2canvas -->
-<script src="{{ url('/public/js') }}/html2canvas.min.js"></script>
-
+<script src="https://www.gstatic.com/firebasejs/5.8.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBfSX_L0LgSt8apo6WtOqFIL3_PAve27HI",
+    authDomain: "qurannlife-5698f.firebaseapp.com",
+    databaseURL: "https://qurannlife-5698f.firebaseio.com",
+    projectId: "qurannlife-5698f",
+    storageBucket: "",
+    messagingSenderId: "1007456883148"
+  };
+  firebase.initializeApp(config);
+</script>
 
 <script>
   window.fbAsyncInit = function() {
@@ -32,3 +33,15 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ url('/public/vendor') }}/jquery/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="{{ url('/public/vendor') }}/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="{{ url('/public/vendor') }}/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="{{ url('/public/js') }}/resume.min.js"></script>
+<!-- html2canvas -->
+<script src="{{ url('/public/js') }}/html2canvas.min.js"></script>
