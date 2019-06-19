fef6b0d2f9b53a2ef2c93f6940647b0e72371557
Nabeel032 <nabeel.farooq@nxb.com.pk>
2019-06-19 19:47:54 +0500

    Facebook Share

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 <meta name="author" content="Talha"/>
 <meta name="_token" content="{{csrf_token()}}"/>
-<title>Quran & Life</title>
 
 <!-- Bootstrap core CSS -->
 <link href="{{ url('/public/vendor') }}/bootstrap/css/bootstrap.min.css" integrity="" rel="stylesheet">
